4fe53447307d27620f45d53ca6baf072000428cb
Adam Moore <adamjamesmoore@live.co.uk>
2020-11-23 13:57:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ const two = 2
 
 var funkyFunction = function() {
   var theFunk = "FUNKY!"
-
+return function(){
     return theFunk
   }
-
+}
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
